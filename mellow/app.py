f7b4cf168ef78bcfa00c6b5d4bca2618b612a7de
Stefan Eng <eng_steff@outlook.com>
2021-09-19 14:48:17 +0200

    feat: Pull tasks from db and display

--- mellow/app.py
+++ mellow/app.py
@@ -1,12 +1,15 @@
 import os
 
-from flask import Flask, render_template
+from flask import render_template, g
+
+from mellow import db
 from mellow.config import app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tasks=db.tasks())
+
 
 def run():
     os.environ['FLASK_ENV'] = 'development'
